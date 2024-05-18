1c7085c4e23cb20a830c65f42d11f63292391fd6
yogesh-gavande <146428784+yogesh-gavande@users.noreply.github.com>
2024-05-18 19:53:47 +0530

    Add files via upload
    Task No : 1 Landing page

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add event listeners to the buttons
+// Get the "Join Now" button element
+const joinNowBtn = document.getElementById('joinNowBtn');
+
+// Add an event listener for the click event
+joinNowBtn.addEventListener('click', function() {
+  // Action to be performed when the button is clicked
+  alert("Thank you for joining!' sham' s Libery");
+});
